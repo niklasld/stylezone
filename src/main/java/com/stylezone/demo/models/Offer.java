3daa85bf7ff380ff89c37e09ce37625f96c0d708
hasan <hhaidari25@gmail.com>
2018-12-04 11:26:27 +0100

    copied data from old repository

---
+++ src/main/java/com/stylezone/demo/models/Offer.java
@@ -0,0 +1,80 @@
+package com.stylezone.demo.models;
+
+import java.util.Date;
+
+public class Offer {
+    private int offerId;
+    private String offerName;
+    private String offerContent;
+    private String offerStart;
+    private String offerEnd;
+
+    public Offer() {
+    }
+
+    public Offer(String offerName, String offerContent, String offerStart, String offerEnd) {
+        this.offerName = offerName;
+        this.offerContent = offerContent;
+        this.offerStart = offerStart;
+        this.offerEnd = offerEnd;
+    }
+
+    public Offer(int offerId, String offerName, String offerContent, String offerStart, String offerEnd) {
+        this.offerId = offerId;
+        this.offerName = offerName;
+        this.offerContent = offerContent;
+        this.offerStart = offerStart;
+        this.offerEnd = offerEnd;
+    }
+
+    public int getOfferId() {
+        return offerId;
+    }
+
+    public void setOfferId(int offerId) {
+        this.offerId = offerId;
+    }
+
+    public String getOfferName() {
+        return offerName;
+    }
+
+    public void setOfferName(String offerName) {
+        this.offerName = offerName;
+    }
+
+    public String getOfferContent() {
+        return offerContent;
+    }
+
+    public void setOfferContent(String offerContent) {
+        this.offerContent = offerContent;
+    }
+
+    public String getOfferStart() {
+        return offerStart;
+    }
+
+    public void setOfferStart(String offerStart) {
+        this.offerStart = offerStart;
+    }
+
+    public String getOfferEnd() {
+        return offerEnd;
+    }
+
+    public void setOfferEnd(String offerEnd) {
+        this.offerEnd = offerEnd;
+    }
+
+    @Override
+    public String toString() {
+        return "Offer{" +
+                "offerId=" + offerId +
+                ", offerName='" + offerName + '\'' +
+                ", offerContent='" + offerContent + '\'' +
+                ", offerStart=" + offerStart +
+                ", offerEnd=" + offerEnd +
+                '}';
+    }
+}
